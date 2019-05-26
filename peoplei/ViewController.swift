05fdc9d436ac4187eda206eb2ecdbf4886e3315f
Andrew Schools <andrewschools@me.com>
2019-05-25 20:40:47 -0400

    Initial Commit

---
+++ peoplei/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  peoplei
+//
+//  Created by Andrew Schools on 5/25/19.
+//  Copyright © 2019 Andrew Schools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
